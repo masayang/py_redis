14d3b4654d4447e044b82a83ab404fd86f7e9468
masayang <masayang.mobile@gmail.com>
2017-08-29 13:18:45 -0700

    test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -20,11 +20,14 @@ class TestIntegration(unittest.TestCase):
         self.cleanup('RedisDict')
         self.cleanup('RedisList')
         self.cleanup('@masayang')
+        self.cleanup('test_board')
 
 
     def cleanup(self, prefix):
         for key in self.sr.keys(prefix + '*'):
             self.sr.delete(key)
+
+
 
     def test_user_creation(self):
         masayang = TwitterUser(
@@ -62,11 +65,40 @@ class TestIntegration(unittest.TestCase):
             result.append(t.id)
         self.assertEqual(result, [u'Tweet:2Fj6QJeQAGRy'])
 
-    def test_score_board(self):
-        board = ScoreBoard(id="test_board", init_items=[
+class TestScoreBoard(unittest.TestCase):
+    def setUp(self):
+        self.sr = StrictRedis(
+            host=redis_config['host'],
+            port=redis_config['port'],
+            db=redis_config['db'],
+            password=redis_config['password']
+        )
+
+        self.board = ScoreBoard(id="test_board", init_items=[
             {'2017-01': 100},
             {'2017-02': 12},
             {'2017-03': 222}
         ])
-        self.assertEquals(len(board), 3)
-        self.assertEquals(board.rank('2017-01'), 1)
+
+    def tearDown(self):
+        self.cleanup('test_board')
+
+    def cleanup(self, prefix):
+        for key in self.sr.keys(prefix + '*'):
+            self.sr.delete(key)
+
+    def test_len(self):
+        self.assertEquals(len(self.board), 3)
+
+    def test_count(self):
+        self.assertEquals(self.board.count(0, 1000), 3)
+        self.assertEquals(self.board.count(100, 1000), 2)
+        self.assertEquals(self.board.count(200, 1000), 1)
+
+    def test_rank(self):
+        self.assertEquals(self.board.rank('2017-01'), 1)
+        self.assertEquals(self.board.rank('2017-02'), 0)
+        self.assertEquals(self.board.rank('2017-03'), 2)
+
+    def test_lexcount(self):
+        self.assertEquals(self.board.lexcount('2', '3'), 0)
