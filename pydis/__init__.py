bb6cd75f950b86694f7eb0874071aa7c0a20b540
Masa Nakamura <Masa Nakamura>
2017-08-30 15:17:27 -0700

    compatiblity for 3.x

--- pydis/__init__.py
+++ pydis/__init__.py
@@ -1,8 +1,9 @@
-from redis_object import RedisObject
-from redis_list import RedisList
-from redis_dict import RedisDict
-from redis_set import RedisSet
-from redis_sortedset import RedisSortedSet
+from __future__ import absolute_import
+from .redis_object import RedisObject
+from .redis_list import RedisList
+from .redis_dict import RedisDict
+from .redis_set import RedisSet
+from .redis_sortedset import RedisSortedSet
 
 __all__ = [
     'RedisObject',
