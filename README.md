69efdb8b83e47ba5f63fd65afef0ab9be0da1653
masayang <masayang.mobile@gmail.com>
2017-08-30 23:46:57 -0700

    version 0.1

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Python wrappers for StrictRedis inspired by https://blog.jverkamp.com/2015/07/16
 
 <a href='https://coveralls.io/github/masayang/py_redis?branch=master'><img src='https://coveralls.io/repos/github/masayang/py_redis/badge.svg?branch=master' alt='Coverage Status' /></a>
 
+## Installation
+
+pip install -u pydis
+
+## Configuration
+
+See redis_settings.py for detail.
+
+## Usage
+
+See samples/__init__.py
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='pydis',
+    packages=['pydis'],
+    version='0.1',
+    description='Python wrappers for StrictRedis',
+    author='masayang',
+    author_email='masayang@msushi.com',
+    url='https://github.com/masayang/py_redis',
+    download_url='https://github.com/masayang/py_redis/archive/0.1.tar.gz',
+    requires=['redis==2.10.6', 'python_dotenv==0.6.5'],
+    keywords=['redis'],
+    classifiers=[],
+)
